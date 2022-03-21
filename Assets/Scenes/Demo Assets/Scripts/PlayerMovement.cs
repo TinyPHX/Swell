54ca81562a3258c0ad83eaf5d78cc3aeb8a2e6d3
mkatic <michaelkatic@gmail.com>
2022-03-20 21:02:14 -0700

    Cleaned up and organized assets and dependencies.

---
+++ Assets/Scenes/Demo Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float rotationForce = 1;
+    public float throttleForce = 1;
+    public float airRollForce = 1;
+    public float angularRange = 120;
+    new Rigidbody rigidbody;
+    float input_rotate = 0;
+    float input_throttle = 0;
+    float input_verticalRoll = 0;
+    float input_horizontalRoll = 0;
+    bool input_altRoll = false;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateControllsKeyboard();
+        UpdateRotation();
+        AirAdjustment();
+    }
+
+    void UpdateControllsKeyboard()
+    {
+        input_rotate = Input.GetAxis("Horizontal_Move");
+        input_throttle = Input.GetAxis("Vertical_Move");
+        input_altRoll = Input.GetAxis("Jump") > 0;
+        input_verticalRoll = input_throttle;
+        input_horizontalRoll = input_rotate;
+    }
+
+    void UpdateRotation()
+    {
+        Quaternion rotation = Quaternion.identity;
+        Vector3 boomboxRotation = Vector3.zero;
+        boomboxRotation.y = -input_rotate * (angularRange / 2);
+        rotation.eulerAngles = boomboxRotation;
+        Quaternion globalRotation = rotation * transform.rotation;
+    
+        if (input_throttle > 0)
+        {
+            if (input_altRoll)
+            {
+                input_throttle *= -.2f;
+            }
+    
+            Vector3 rotatedtThrottleForce = globalRotation * Vector3.forward * throttleForce * input_throttle;
+            Vector3 throttleOrigin = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    
+            rigidbody.AddForceAtPosition(rotatedtThrottleForce, throttleOrigin);
+        }
+        
+        Vector3 veloocity = Quaternion.Inverse(transform.rotation) * rigidbody.velocity;
+        veloocity.x *= .9f;
+        rigidbody.velocity = transform.rotation * veloocity;
+
+    }
+
+    void AirAdjustment()
+    {
+        rigidbody.AddRelativeTorque(Time.deltaTime * input_verticalRoll * airRollForce, 0, 0);
+
+        if (!input_altRoll)
+        {
+            rigidbody.AddRelativeTorque(0, Time.deltaTime * input_horizontalRoll * airRollForce, 0);
+        }
+        else
+        {
+            rigidbody.AddRelativeTorque(0, 0, Time.deltaTime * -input_horizontalRoll * airRollForce);
+        }
+    }
+}
---
+++ Assets/Scenes/Demo Assets/Scripts/SmoothFollow.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/// <summary>
+/// This class is based on a script from "Unity/Standard Assets (Mobile)/Scripts/SmoothFollow.cs"
+/// </summary>
+public class SmoothFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float smoothTime = 0.1f;
+    
+    private Vector3 velocity = Vector3.zero;
+    
+    void FixedUpdate()
+    {
+        MoveTowardsTarget();
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
+    }
+
+    public void MoveTowardsTarget()
+    {
+        float cameraDistance = Vector3.Distance(transform.position, target.transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, cameraDistance * smoothTime);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, target.transform.rotation, cameraDistance * smoothTime * 10);
+    }
+
+    public void MoveToTarget()
+    {
+        transform.position = target.position;
+    }
+
+    public void MoveTargetToHere()
+    {
+        target.position = transform.position;
+    }
+}
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+[CustomEditor(typeof(SmoothFollow))]
+public class SmoothFollow2DEditor : Editor
+{
+    SmoothFollow smoothFollow;
+
+    void OnEnable()
+    {
+        smoothFollow = FindObjectOfType<SmoothFollow>();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Move to target"))
+        {
+            smoothFollow.MoveToTarget();
+        }
+        
+        if (GUILayout.Button("Move target here"))
+        {
+            smoothFollow.MoveTargetToHere();
+        }
+    }
+}
+#endif
